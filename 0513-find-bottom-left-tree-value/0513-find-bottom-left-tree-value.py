9c974617f046de41d60b9e27a2be2541273ad034
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2022-10-19 09:40:59 +0530

    Time: 49 ms (89.81%), Space: 16.2 MB (66.11%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -6,26 +6,13 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-      if not(root):
-        return 
-      
-      queue=[[root]]
-      
-      ans=[]
+    
+      queue=[root]
       
       while queue:
         
-        level=queue.pop(0)
-        ans.append([node.val for node in level])
+        p=queue.pop(0)
         
-        nested_level=[]
-        
-        for node in level:
-          
-          if node.left:
-            nested_level.append(node.left)
-          if node.right:
-            nested_level.append(node.right)
-        if nested_level:
-          queue.append(nested_level)
-      return ans[-1][0]
+        if p.right: queue.append(p.right)
+        if p.left: queue.append(p.left)
+      return p.val
