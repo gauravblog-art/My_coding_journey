e39e860d2096e398eaf8abdba29abfe437794f87
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2022-11-01 09:19:56 +0530

    Time: 95 ms (52.60%), Space: 13.8 MB (76.80%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,15 +2,15 @@ class Solution:
     def romanToInt(self, s: str) -> int:
       roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
       
+      s=s.replace('IV', 'IIII')
+      s=s.replace('XL', 'XXXX')
+      s=s.replace('CD', 'CCCC')
+      s=s.replace('IX', 'VIIII')
+      s=s.replace('XC', 'LXXXX')
+      s=s.replace('CM', 'DCCCC')
+      
       ans=0
-      for i in range(len(s)-1,-1,-1):
-        
-        num=roman[s[i]]
-        
-        if 4*num<ans:
-          ans-=num
-        else:
-          
-          ans+=num
-      return ans
-        
+      
+      for i in s:
+        ans+=roman[i]
+      return ans  
