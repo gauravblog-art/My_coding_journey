67012cfdc0af679b0a6e85492f580e16bb98f2fb
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2022-11-03 10:06:30 +0530

    Time: 41 ms (82.96%), Space: 13.9 MB (59.43%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+    
+      # solving by recursion 
+      
+      if digits[-1]<9:
+        digits[-1]+=1
+        
+        return digits
+      elif len(digits)==1 and digits[-1]==9:
+        
+        return [1,0]
+      
+      digits[-1]=0
+      digits[0:-1]=self.plusOne(digits[0:-1])
+      return digits
+      
+      
