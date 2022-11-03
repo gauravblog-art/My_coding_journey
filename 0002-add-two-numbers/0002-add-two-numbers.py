c16df19a28fcb574c774512dd865b67e7066487e
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2022-11-03 21:14:59 +0530

    Time: 154 ms (31.30%), Space: 14 MB (43.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,18 +12,22 @@ class Solution:
       
       while l1 or l2 or carry:
         
-        v1=l1.val if l1 else 0
-        v2=l2.val if l2 else 0
+        v1=0
+        v2=0
         
+        if l1:
+          v1=l1.val
+          l1=l1.next
+        if l2:
+          v2=l2.val
+          l2=l2.next
         
-        val=v1+v2+carry
-        carry=val//10
-        val=val%10
+        s=v1+v2+carry
+        carry=s//10
+        val=(s)%10 
         
         temp.next=ListNode(val)
         temp=temp.next
         
-        l1=l1.next if l1 else None
-        l2=l2.next if l2 else None
       return dummy.next
         
