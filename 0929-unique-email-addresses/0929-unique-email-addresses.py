1a93ca7f5411d36552c165b180c28d7302b91d69
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2022-11-08 10:07:13 +0530

    Time: 176 ms (14.32%), Space: 14.1 MB (35.93%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -4,10 +4,18 @@ class Solution:
       unique=set()
         
       for e in emails:
-        local,domain= e.split("@")
-        local=local.split('+')[0]
-        local=local.replace(".", "")
+        i=0
+        local=''
+        domain=''
+        while e[i] not in ['@', '+']:
+          
+          if e[i]!='.':
+            local+=e[i]
+          i+=1
+        while e[i]!='@':
+          i+=1
         
+        domain+=e[i+1:]
         unique.add((local,domain))
         
       return len(unique)
